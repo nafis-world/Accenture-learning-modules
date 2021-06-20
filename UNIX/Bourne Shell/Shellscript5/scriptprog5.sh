76f0932dea84843d2672f2434defecb8d9368180
Nafis Ali <57516209+nafis-world@users.noreply.github.com>
2021-06-20 12:29:05 +0530

    Updated Shellscript5
    Removed spaces.

--- UNIX/Bourne Shell/Shellscript5/scriptprog5.sh
+++ UNIX/Bourne Shell/Shellscript5/scriptprog5.sh
@@ -6,8 +6,8 @@ echo "Not a positive number"
 else
 while [[ $a -gt 0 ]]
 do 
-    k=$(( $a % 10 ))
-    a=$(( $a / 10 ))
+    k=$(( $a%10 ))
+    a=$(( $a/10 ))
     s=$(( $k + $s ))
 done
 echo "Sum of digit for given number is $s"
